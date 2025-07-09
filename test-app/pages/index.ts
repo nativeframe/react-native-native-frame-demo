51ad34a4c8d339c36f8fbf6fff5fe5688c1eba46
Antonio Johnson <antonio@jtigernova.com>
2025-07-09 15:05:28 -0500

    fix: broadcast

--- test-app/pages/index.ts
+++ test-app/pages/index.ts
@@ -1,7 +1,7 @@
 export class ReactNativeBrowserAdapter {
   static INSTANCE = new ReactNativeBrowserAdapter({
-    platform: "ReactNative",
-    userAgent: "ReactNative",
+    platform: 'ReactNative',
+    userAgent: 'ReactNative',
   });
 
   readonly platform: string;
@@ -30,7 +30,7 @@ export class ReactNativeBrowserAdapter {
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const globalThis: any = global;
 if (globalThis?.navigator != null && globalThis.navigator.userAgent == null) {
-  Object.defineProperty(globalThis.navigator, "userAgent", {
+  Object.defineProperty(globalThis.navigator, 'userAgent', {
     enumerable: true,
     configurable: true,
     get(): any {
@@ -42,7 +42,7 @@ if (globalThis?.navigator != null && globalThis.navigator.userAgent == null) {
   });
 }
 
-const adapter = require("@video/video-client-core").adapter;
-const ReactNativeDevice = require("./reactnative-device").ReactNativeDevice;
+const adapter = require('@video/video-client-core').adapter;
+const ReactNativeDevice = require('./reactnative-device').ReactNativeDevice;
 
 adapter.implement(new ReactNativeDevice());
